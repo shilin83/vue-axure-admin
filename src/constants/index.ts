390dd1f6a963c70467fd296a99af002176b81864
shilin <shilin_83@qq.com>
2026-06-30 19:41:09 +0800

    ✨ 应用常量

---
+++ src/constants/index.ts
@@ -0,0 +1,19 @@
+const CURRENT_PAGE = 1;
+
+const PAGE_SIZE = 10;
+
+const DELAY_TIME = 2000;
+
+const AUTH_KEY = "auth";
+
+const DEFAULT_PASSWORD = "123456";
+
+const USER_AVATAR = "circle-flags:cn";
+
+const DB_NAME = "axure";
+
+const NO_DATA = "暂无数据";
+
+const SERVER_ERROR = "服务器错误";
+
+export { AUTH_KEY, DEFAULT_PASSWORD, DELAY_TIME, DB_NAME, USER_AVATAR, CURRENT_PAGE, PAGE_SIZE, NO_DATA, SERVER_ERROR };
